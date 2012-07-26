33fd458f3c4059d0efd993d907f9147f4715fe41
Tony Trinh <tony19@gmail.com>
2012-07-25 20:04:06 -0400

    First pass on maven build

--- logback-examples/src/main/resources/setClasspath.sh
+++ logback-examples/src/main/resources/setClasspath.sh
@@ -6,9 +6,9 @@
 
 LB_HOME=/SET/THIS/PARAMETER/TO/THE/DIRECTORY/WHERE/YOU/INSTALLED/LOGBACK
 
-CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-classic-${project.version}.jar"
-CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-core-${project.version}.jar"
-CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-examples/logback-examples-${project.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-android-classic-${project.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-android-core-${project.version}.jar"
+CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-examples/logback-android-examples-${project.version}.jar"
 CLASSPATH="${CLASSPATH}:${LB_HOME}/logback-examples/lib/slf4j-api-${slf4j.version}.jar"
 
 export CLASSPATH
