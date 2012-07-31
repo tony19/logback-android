4cb54230b478338d280b42de8eb35371180b0afe
Tony Trinh <tony19@gmail.com>
2012-07-31 05:15:17 -0400

    Add dryrun flag to release.sh

--- release.sh
+++ release.sh
@@ -27,16 +27,23 @@ version=${version%-SNAPSHOT}
 outf=logback-android-${version}.jar
 outdir=$PWD/target
 readme=$PWD/README.md
-
+if [ "x$1" == "xdryrun" ]; then
+  echo "[dryrun] just a test!"
+  dryrun=true
+fi
 echo "Starting release process for logback-android ${version}..."
 
 #
 # Build the JAR and print its MD5. The last line uses GNU sed (gsed)
 # to update the README with the current release version.
 #
+if [ ! ${dryrun} ]; then
 mvn release:clean || exit 1
 mvn -Dtag=v_${version} release:prepare || exit 1
 mvn -Dtag=v_${version} release:perform || exit 1
+else
+echo '[dryrun] skip mvn release...'
+fi
 
 mvn versions:set -DnewVersion=${version}
 mvn clean install -DskipTests
@@ -48,8 +55,12 @@ gsed -i -e "s/logback-android-[^j]*\.jar/${outf}/" \
 -e "s/[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+/${version}/" \
 -e "s/\(logback-android.*MD5\:\).*/\1 \`$(md5 ${outdir}/${outf} | awk '{print $4}')\`)/" ${readme}
 
+if [ ! ${dryrun} ]; then
 git add ${readme}
 git commit -m "Update README for release ${version}"
+else
+echo '[dryrun] skip commit README...'
+fi
 
 echo "Generating javadoc..."
 mvn javadoc:javadoc
@@ -59,12 +70,10 @@ git clone -b gh-pages https://github.com/tony19/logback-android.git gh-pages
 cd gh-pages
 rm -rf doc/${version}
 mv ${outdir}/site/apidocs doc/${version}
-git add doc/${version}
 
 echo "Updating index.html..."
 gsed -i -e "s/logback-android-[^j]*\.jar/${outf}/g" \
 -e "s/[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+/${version}/g" index.html
-git add index.html
 
 insdiv="<div id=\"${version}\" class=\"release\">\n\
 \t\t<div class=\"version\">${version}</div>\n\
@@ -80,9 +89,21 @@ if [ $? -gt 0 ]; then
 	echo "Updating changelog.html..."
 	gsed -i -e "s@<div id=[\"\']notes[\"\']>@&\n\t${insdiv}@" changelog.html
 fi
+
+if [ ! ${dryrun} ]; then
+git add index.html
+git add doc/${version}
 git add changelog.html
 
 # checkin changes to the web pages
 git commit -m "release ${version}"
 
+else
+echo '[dryrun] skip commit gh-pages...'
+fi
+
+if [ ! ${dryrun} ]; then
 echo Done. Push changes to GitHub!!
+else
+echo Done...just a dryrun!!
+fi
