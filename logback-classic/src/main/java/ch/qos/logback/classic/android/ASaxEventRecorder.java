3bf123122d79397496c78c171a5a79704e3258d7
Tony Trinh <tony19@gmail.com>
2012-11-13 23:03:54 -0600

    Fix whitespace
    This change replaces tabs with spaces in ASaxEventRecorder
    to match convention. It also deletes whitespace chars from
    blank lines.

--- logback-classic/src/main/java/ch/qos/logback/classic/android/ASaxEventRecorder.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/android/ASaxEventRecorder.java
@@ -36,231 +36,231 @@ import ch.qos.logback.core.joran.spi.JoranException;
  * @author Anthony Trinh
  */
 public class ASaxEventRecorder extends SaxEventRecorder {
-	private int holderForStartAndLength[] = new int[2];
-	private StatePassFilter filter = new StatePassFilter();
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param context logger context
-	 */
-	public ASaxEventRecorder(Context context) {
-		super();
-		setContext(context);
-	}
+  private int holderForStartAndLength[] = new int[2];
+  private StatePassFilter filter = new StatePassFilter();
 
-	/**
-	 * Sets a filter so that only sub-elements of a specific element 
-	 * are captured
-	 * 
-	 * <p>
-	 * For example, if the desired elements were inside
-	 * <br>
-	 * <blockquote>{@code <x><y>}</blockquote> 
-	 * and the input were 
-	 * <br>
-	 * <blockquote>{@code <x><y><a/><b/><c/></x></y>}</blockquote>
-	 * the filter would pass
-	 * <br>
-	 * <blockquote>{@code <a/><b/><c/>}</blockquote>
-	 * 
-	 * The call in this example would be: {@code setFilter("x", "y")}.
-	 * 
-	 * @param names names of elements leading to the target elements;
-	 * use {@code null} to disable filtering (capture all events)
-	 */
-	public void setFilter(String... names) {
-		filter = new StatePassFilter(names);
-	}
-	
-	/**
-	 * Parses SAX events from a compressed Android XML resource
-	 * 
-	 * @param src input source pointing to a compressed Android XML resource
-	 */
-	@Override
-	public List<SaxEvent> recordEvents(InputSource src) throws JoranException {
-		InputStream stream = src.getByteStream();
-		if (stream == null) {
-			throw new IllegalArgumentException("Input source must specify an input stream");
-		}
+  /**
+   * Constructor
+   * 
+   * @param context logger context
+   */
+  public ASaxEventRecorder(Context context) {
+    super();
+    setContext(context);
+  }
 
-		List<SaxEvent> events = null;
-		try {
-			AXmlResourceParser xpp = new AXmlResourceParser(stream);
-			
-			int eventType = -1;
-			while ((eventType = xpp.next()) > -1) {
-				if (XmlPullParser.START_DOCUMENT == eventType) {
-					filter.reset();
-					startDocument(xpp);
-				} else if (XmlPullParser.END_DOCUMENT == eventType) {
-					filter.reset();
-					endDocument();
-					break;
-				} else if (XmlPullParser.START_TAG == eventType) {
-					startElement(xpp);
-				} else if (XmlPullParser.END_TAG == eventType) {
-					endElement(xpp);
-				} else if (XmlPullParser.TEXT == eventType) {
-					characters(xpp);
-				}
-			}
-			
-			events = getSaxEventList();
+  /**
+   * Sets a filter so that only sub-elements of a specific element 
+   * are captured
+   * 
+   * <p>
+   * For example, if the desired elements were inside
+   * <br>
+   * <blockquote>{@code <x><y>}</blockquote> 
+   * and the input were 
+   * <br>
+   * <blockquote>{@code <x><y><a/><b/><c/></x></y>}</blockquote>
+   * the filter would pass
+   * <br>
+   * <blockquote>{@code <a/><b/><c/>}</blockquote>
+   * 
+   * The call in this example would be: {@code setFilter("x", "y")}.
+   * 
+   * @param names names of elements leading to the target elements;
+   * use {@code null} to disable filtering (capture all events)
+   */
+  public void setFilter(String... names) {
+    filter = new StatePassFilter(names);
+  }
 
-		} catch (Exception e) {
-			addError(e.getMessage(), e);
-	    throw new JoranException("Can't parse Android XML resource", e);
-		}
-		return events;
-	}
+  /**
+   * Parses SAX events from a compressed Android XML resource
+   * 
+   * @param src input source pointing to a compressed Android XML resource
+   */
+  @Override
+  public List<SaxEvent> recordEvents(InputSource src) throws JoranException {
+    InputStream stream = src.getByteStream();
+    if (stream == null) {
+      throw new IllegalArgumentException("Input source must specify an input stream");
+    }
 
-	/**
-	 * Processes the START_DOCUMENT event
-	 *  
-	 * @param xpp parser that contains the event
-	 */
-	private void startDocument(XmlPullParser xpp) {
-		super.startDocument();
-		super.setDocumentLocator(new LocatorImpl());
-	}
+    List<SaxEvent> events = null;
+    try {
+      AXmlResourceParser xpp = new AXmlResourceParser(stream);
 
-	/**
-	 * Processes the TEXT event
-	 * 
-	 * @param xpp parser that contains the event
-	 */
-	private void characters(XmlPullParser xpp) {
-		if (filter.passed()) {
-			char ch[] = xpp.getTextCharacters(holderForStartAndLength);
-			int start = holderForStartAndLength[0];
-			int length = holderForStartAndLength[1];
-			super.characters(ch, start, length);
-		}
-	}
+      int eventType = -1;
+      while ((eventType = xpp.next()) > -1) {
+        if (XmlPullParser.START_DOCUMENT == eventType) {
+          filter.reset();
+          startDocument(xpp);
+        } else if (XmlPullParser.END_DOCUMENT == eventType) {
+          filter.reset();
+          endDocument();
+          break;
+        } else if (XmlPullParser.START_TAG == eventType) {
+          startElement(xpp);
+        } else if (XmlPullParser.END_TAG == eventType) {
+          endElement(xpp);
+        } else if (XmlPullParser.TEXT == eventType) {
+          characters(xpp);
+        }
+      }
 
-	/**
-	 * Process the END_ELEMENT event
-	 * 
-	 * @param xpp parser that contains the event
-	 */
-	private void endElement(XmlPullParser xpp) {
-		String name = xpp.getName();
-		if (filter.checkEnd(name)) {
-			endElement(xpp.getNamespace(), name, name);
-		}
-	}
+      events = getSaxEventList();
 
-	/**
-	 * Processes the START_ELEMENT event
-	 *  
-	 * @param xpp parser that contains the event
-	 */
-	private void startElement(XmlPullParser xpp) {
-		String name = xpp.getName();
-		if (filter.checkStart(name)) {
-			AttributesImpl atts = new AttributesImpl();
-			for (int i = 0; i < xpp.getAttributeCount(); i++) {
-				atts.addAttribute(xpp.getAttributeNamespace(i),
-				    xpp.getAttributeName(i), xpp.getAttributeName(i),
-				    xpp.getAttributeType(i), xpp.getAttributeValue(i));
-			}
-			startElement(xpp.getNamespace(), name, name, atts);
-		}
-	}
-	
-	/**
-	 * Filter that passes start-tags and end-tags within a specific
-	 * set of tags. 
-	 * 
-	 * <p>
-	 * This is useful in an XML pull parser for capturing XML sub-elements 
-	 * of a specific element. For example, if the desired elements were in 
-	 * <br>
-	 * <blockquote>{@code <x><y>}</blockquote> 
-	 * and the input were 
-	 * <br>
-	 * <blockquote>{@code <x><y><a/><b/><c/></x></y>}</blockquote>
-	 * the filter would pass
-	 * <br>
-	 * <blockquote>{@code <a/><b/><c/>}</blockquote>
-	 * 
-	 * The initialization in this example would be: {@code new StatePassFilter("x", "y")}.
-	 */
-	class StatePassFilter {
-		private String[] _states;
-		private int _depth = 0;
-		
-		public StatePassFilter(String... states) {
-			_states = states == null ? new String[0] : states;
-		}
-		
-		/**
-		 * Checks if a start-tag element name passes the filter. If the
-		 * name matches the filter's current state, the filter depth
-		 * is advanced.
-		 *  
-		 * @param name element name to check
-		 * @return {@true code} if passed; {@code false} otherwise
-		 */
-		public boolean checkStart(String name) {
-			if (_depth == _states.length) {
-				return true;
-			} else if (name.equals(_states[_depth])) {
-				_depth++;
-			}
-			return false;
-		}
-		
-		/**
-		 * Checks if an end-tag element name passes the filter. If the
-		 * name matches the filter's current state, the filter depth
-		 * is decremented.
-		 * 
-		 * @param name element name to check
-		 * @return {@true code} if passed; {@code false} otherwise
-		 */
-		public boolean checkEnd(String name) {
-			if ((_depth > 0) && name.equals(_states[_depth - 1])) {
-				_depth--;
-				return false;
-			}
-			return _depth == _states.length;
-		}
-		
-		/**
-		 * Gets the number of states in the filter
-		 * 
-		 * @return state count
-		 */
-		public int size() {
-			return _states.length;
-		}
-		
-		/**
-		 * Gets the current depth (state) into the filter
-		 * 
-		 * @return depth
-		 */
-		public int depth() {
-			return _depth;
-		}
-		
-		/**
-		 * Resets the depth (state)
-		 */
-		public void reset() {
-			_depth = 0;
-		}
-		
-		/**
-		 * Gets the pass state. Equivalent to {@code depth() == size()}.
-		 * 
-		 * @return {@code true} if passed; {@code false} otherwise
-		 */
-		public boolean passed() {
-			return _depth == _states.length;
-		}
-	}
+    } catch (Exception e) {
+      addError(e.getMessage(), e);
+      throw new JoranException("Can't parse Android XML resource", e);
+    }
+    return events;
+  }
+
+  /**
+   * Processes the START_DOCUMENT event
+   *  
+   * @param xpp parser that contains the event
+   */
+  private void startDocument(XmlPullParser xpp) {
+    super.startDocument();
+    super.setDocumentLocator(new LocatorImpl());
+  }
+
+  /**
+   * Processes the TEXT event
+   * 
+   * @param xpp parser that contains the event
+   */
+  private void characters(XmlPullParser xpp) {
+    if (filter.passed()) {
+      char ch[] = xpp.getTextCharacters(holderForStartAndLength);
+      int start = holderForStartAndLength[0];
+      int length = holderForStartAndLength[1];
+      super.characters(ch, start, length);
+    }
+  }
+
+  /**
+   * Process the END_ELEMENT event
+   * 
+   * @param xpp parser that contains the event
+   */
+  private void endElement(XmlPullParser xpp) {
+    String name = xpp.getName();
+    if (filter.checkEnd(name)) {
+      endElement(xpp.getNamespace(), name, name);
+    }
+  }
+
+  /**
+   * Processes the START_ELEMENT event
+   *  
+   * @param xpp parser that contains the event
+   */
+  private void startElement(XmlPullParser xpp) {
+    String name = xpp.getName();
+    if (filter.checkStart(name)) {
+      AttributesImpl atts = new AttributesImpl();
+      for (int i = 0; i < xpp.getAttributeCount(); i++) {
+        atts.addAttribute(xpp.getAttributeNamespace(i),
+            xpp.getAttributeName(i), xpp.getAttributeName(i),
+            xpp.getAttributeType(i), xpp.getAttributeValue(i));
+      }
+      startElement(xpp.getNamespace(), name, name, atts);
+    }
+  }
+
+  /**
+   * Filter that passes start-tags and end-tags within a specific
+   * set of tags. 
+   * 
+   * <p>
+   * This is useful in an XML pull parser for capturing XML sub-elements 
+   * of a specific element. For example, if the desired elements were in 
+   * <br>
+   * <blockquote>{@code <x><y>}</blockquote> 
+   * and the input were 
+   * <br>
+   * <blockquote>{@code <x><y><a/><b/><c/></x></y>}</blockquote>
+   * the filter would pass
+   * <br>
+   * <blockquote>{@code <a/><b/><c/>}</blockquote>
+   * 
+   * The initialization in this example would be: {@code new StatePassFilter("x", "y")}.
+   */
+  class StatePassFilter {
+    private String[] _states;
+    private int _depth = 0;
+
+    public StatePassFilter(String... states) {
+      _states = states == null ? new String[0] : states;
+    }
+
+    /**
+     * Checks if a start-tag element name passes the filter. If the
+     * name matches the filter's current state, the filter depth
+     * is advanced.
+     *  
+     * @param name element name to check
+     * @return {@true code} if passed; {@code false} otherwise
+     */
+    public boolean checkStart(String name) {
+      if (_depth == _states.length) {
+        return true;
+      } else if (name.equals(_states[_depth])) {
+        _depth++;
+      }
+      return false;
+    }
+
+    /**
+     * Checks if an end-tag element name passes the filter. If the
+     * name matches the filter's current state, the filter depth
+     * is decremented.
+     * 
+     * @param name element name to check
+     * @return {@true code} if passed; {@code false} otherwise
+     */
+    public boolean checkEnd(String name) {
+      if ((_depth > 0) && name.equals(_states[_depth - 1])) {
+        _depth--;
+        return false;
+      }
+      return _depth == _states.length;
+    }
+
+    /**
+     * Gets the number of states in the filter
+     * 
+     * @return state count
+     */
+    public int size() {
+      return _states.length;
+    }
+
+    /**
+     * Gets the current depth (state) into the filter
+     * 
+     * @return depth
+     */
+    public int depth() {
+      return _depth;
+    }
+
+    /**
+     * Resets the depth (state)
+     */
+    public void reset() {
+      _depth = 0;
+    }
+
+    /**
+     * Gets the pass state. Equivalent to {@code depth() == size()}.
+     * 
+     * @return {@code true} if passed; {@code false} otherwise
+     */
+    public boolean passed() {
+      return _depth == _states.length;
+    }
+  }
 }
