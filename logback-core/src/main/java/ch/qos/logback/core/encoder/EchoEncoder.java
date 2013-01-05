9319a21595f48dfc3f983eaaf7171bd5eaa8a193
Tony Trinh <tony19@gmail.com>
2013-01-05 02:43:12 -0600

    Remove unwritten fields in EchoEncoder
    This change deletes the fileHeader and fileFooter fields from EchoEncoder.
    They were not written, so the code that null-checks them has also been
    deleted.
    
    LBANDROID-7

--- logback-core/src/main/java/ch/qos/logback/core/encoder/EchoEncoder.java
+++ logback-core/src/main/java/ch/qos/logback/core/encoder/EchoEncoder.java
@@ -20,9 +20,6 @@ import ch.qos.logback.core.CoreConstants;
 
 public class EchoEncoder<E> extends EncoderBase<E> {
 
-  String fileHeader;
-  String fileFooter;
-
   public void doEncode(E event) throws IOException {
     String val = event + CoreConstants.LINE_SEPARATOR;
     outputStream.write(val.getBytes());
@@ -31,17 +28,9 @@ public class EchoEncoder<E> extends EncoderBase<E> {
   }
 
   public void close() throws IOException {
-    if (fileFooter == null) {
-      return;
-    }
-    outputStream.write(fileFooter.getBytes());
   }
 
   public void init(OutputStream os) throws IOException {
     super.init(os);
-    if (fileHeader == null) {
-      return;
-    }
-    outputStream.write(fileHeader.getBytes());
   }
 }
