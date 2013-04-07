c271b8f831d0011f52ebcf13f144d013fd3f25ed
Tony Trinh <tony19@gmail.com>
2013-04-06 19:50:13 -0500

    release 1.0.8-1

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,2 @@
 // assumes Jquery is loaded at this point
-
-$(function() {
-
-	$(".expander").expander();
-	$(".zoom").fancybox({
-		'titlePosition'	: 'inside',
-		'transitionIn'	: 'elastic',
-		'transitionOut'	: 'elastic',
-		'overlayColor'	: '#000',
-		'overlayOpacity': 0.8
-	});
-
-	$("a#changelog").fancybox();
-});
-
 $.trackPage('UA-27964159-1')
