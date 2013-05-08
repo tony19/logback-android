c18a27f6619d9c9967aeb8de9d50d59a6bac2bae
Tony Trinh <tony19@gmail.com>
2013-05-07 20:27:07 -0500

    Add option to make release jar
    Modified makejar.sh to take a parameter that specifies the release
    version of the uber jar (instead of the default: debug build)

--- makejar.sh
+++ makejar.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 #
-# This script creates the uber jar with debug symbols. The tests are skipped.
+# This script creates the uber jar (with debug symbols). The tests are skipped.
 # The jar will be stored in the "./target" directory:
 #
 #   ./target/logback-android-<version>.jar
 #
 
+# Usage: ./makejar.sh [-r]
+#
+#  Options:
+#
+#  -r   make release build (default: false)
+#
+profile="debug"
+
+if [ "x$1" == "x-r" ];
+then
+  profile="release"
+fi
+
 set -e
 
-mvn -P debug clean install -DskipTests=true
+mvn -P $profile clean install -DskipTests=true
 mvn -f pom-uber.xml package
