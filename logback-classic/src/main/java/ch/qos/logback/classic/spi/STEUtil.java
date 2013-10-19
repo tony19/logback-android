3bddfe7b13ab7b96daf79dcf1f073fbc3b059f68
Tony Trinh <tony19@gmail.com>
2013-10-19 00:57:15 -0500

    Remove unused function in STEUtil

--- logback-classic/src/main/java/ch/qos/logback/classic/spi/STEUtil.java
+++ logback-classic/src/main/java/ch/qos/logback/classic/spi/STEUtil.java
@@ -15,29 +15,6 @@ package ch.qos.logback.classic.spi;
 
 public class STEUtil {
 
-  
-  static int UNUSED_findNumberOfCommonFrames(StackTraceElement[] steArray,
-      StackTraceElement[] otherSTEArray) {
-    if (otherSTEArray == null) {
-      return 0;
-    }
-
-    int steIndex = steArray.length - 1;
-    int parentIndex = otherSTEArray.length - 1;
-    int count = 0;
-    while (steIndex >= 0 && parentIndex >= 0) {
-      if (steArray[steIndex].equals(otherSTEArray[parentIndex])) {
-        count++;
-      } else {
-        break;
-      }
-      steIndex--;
-      parentIndex--;
-    }
-    return count;
-  }
-  
-  
   static int findNumberOfCommonFrames(StackTraceElement[] steArray,
       StackTraceElementProxy[] otherSTEPArray) {
     if (otherSTEPArray == null) {
