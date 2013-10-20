7e0ad57b9532c60aa098f06f69954c8a965aa841
Tony Trinh <tony19@gmail.com>
2013-10-20 01:15:40 -0500

    Fix ResilientOutputStreamTest
    The output dir prefix did not always exist when this test was run in
    CloudBees Jenkins. This patch ensures the directory is created before
    the test is run.

--- logback-core/src/test/java/ch/qos/logback/core/recovery/ResilientOutputStreamTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/recovery/ResilientOutputStreamTest.java
@@ -17,7 +17,8 @@ import ch.qos.logback.core.Context;
 import ch.qos.logback.core.ContextBase;
 import ch.qos.logback.core.testUtil.RandomUtil;
 import ch.qos.logback.core.util.CoreTestConstants;
-import ch.qos.logback.core.util.StatusPrinter;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -33,25 +34,29 @@ public class ResilientOutputStreamTest {
   int diff = RandomUtil.getPositiveInt();
   Context context = new ContextBase();
 
+  @Before
+  public void setup() {
+    new File(CoreTestConstants.OUTPUT_DIR_PREFIX).mkdirs();
+  }
 
-   @Test
-   public void verifyRecuperationAfterFailure() throws Exception {
-     File file = new File(CoreTestConstants.OUTPUT_DIR_PREFIX+"resilient"+diff+".log");
-     ResilientFileOutputStream rfos = new ResilientFileOutputStream(file, true);
-     rfos.setContext(context);
+  @Test
+  public void verifyRecuperationAfterFailure() throws Exception {
+    File file = new File(CoreTestConstants.OUTPUT_DIR_PREFIX+"resilient"+diff+".log");
+    ResilientFileOutputStream rfos = new ResilientFileOutputStream(file, true);
+    rfos.setContext(context);
 
-     ResilientFileOutputStream spy = spy(rfos);
+    ResilientFileOutputStream spy = spy(rfos);
 
-     spy.write("a".getBytes());
-     spy.flush();
+    spy.write("a".getBytes());
+    spy.flush();
 
-     spy.getChannel().close();
-     spy.write("b".getBytes());
-     spy.flush();
-     Thread.sleep(RecoveryCoordinator.BACKOFF_COEFFICIENT_MIN+10);
-     spy.write("c".getBytes());
-     spy.flush();
-     verify(spy).openNewOutputStream();
+    spy.getChannel().close();
+    spy.write("b".getBytes());
+    spy.flush();
+    Thread.sleep(RecoveryCoordinator.BACKOFF_COEFFICIENT_MIN+10);
+    spy.write("c".getBytes());
+    spy.flush();
+    verify(spy).openNewOutputStream();
 
    }
 
