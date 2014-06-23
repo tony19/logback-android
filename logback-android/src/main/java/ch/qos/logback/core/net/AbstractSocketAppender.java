eda802b41769b4a5a24b6b9e68e223e6667b20ea
Sebastian Gröbler <s.groebler@googlemail.com>
2014-06-23 13:13:46 +0800

    promoted socket connector to become a member to reduce overhead when creating socket connection

--- logback-android/src/main/java/ch/qos/logback/core/net/AbstractSocketAppender.java
+++ logback-android/src/main/java/ch/qos/logback/core/net/AbstractSocketAppender.java
@@ -85,6 +85,7 @@ public abstract class AbstractSocketAppender<E> extends AppenderBase<E>
   
   private BlockingDeque<E> deque;
   private String peerId;
+  private SocketConnector connector;
   private Future<?> task;
 
   private volatile Socket socket;
@@ -145,6 +146,7 @@ public abstract class AbstractSocketAppender<E> extends AppenderBase<E>
     if (errorCount == 0) {
       deque = queueFactory.newLinkedBlockingDeque(queueSize);
       peerId = "remote peer " + remoteHost + ":" + port + ": ";
+      connector = createConnector(address, port, 0, reconnectionDelay.getMilliseconds());
       task = getContext().getExecutorService().submit(new Runnable() {
         public void run() {
           connectSocketAndDispatchEvents();
@@ -185,8 +187,6 @@ public abstract class AbstractSocketAppender<E> extends AppenderBase<E>
   private void connectSocketAndDispatchEvents() {
     try {
       while (!Thread.currentThread().isInterrupted()) {
-        SocketConnector connector = createConnector(address, port, 0, reconnectionDelay.getMilliseconds());
-
         socket = connector.call();
         if(socket == null)
           break;
@@ -217,10 +217,8 @@ public abstract class AbstractSocketAppender<E> extends AppenderBase<E>
     return objectWriter;
   }
 
-  private SocketConnector createConnector(InetAddress address, int port,
-                                          int initialDelay, long retryDelay) {
-    SocketConnector connector = newConnector(address, port, initialDelay,
-            retryDelay);
+  private SocketConnector createConnector(InetAddress address, int port, int initialDelay, long retryDelay) {
+    SocketConnector connector = newConnector(address, port, initialDelay, retryDelay);
     connector.setExceptionHandler(this);
     connector.setSocketFactory(getSocketFactory());
     return connector;
@@ -273,8 +271,7 @@ public abstract class AbstractSocketAppender<E> extends AppenderBase<E>
    * @param retryDelay delay before a reconnection attempt
    * @return socket connector
    */
-  protected SocketConnector newConnector(InetAddress address,
-                                         int port, long initialDelay, long retryDelay) {
+  protected SocketConnector newConnector(InetAddress address, int port, long initialDelay, long retryDelay) {
     return new DefaultSocketConnector(address, port, initialDelay, retryDelay);
   }
 
