980201c1cb7070f83dcb6bf60cf793c8db904e37
Tony Trinh <tony19@gmail.com>
2016-04-01 09:28:46 -0500

    :zap: Remove unused Gaffer test constant

--- logback-classic/src/test/java/ch/qos/logback/classic/ClassicTestConstants.java
+++ logback-classic/src/test/java/ch/qos/logback/classic/ClassicTestConstants.java
@@ -19,14 +19,11 @@ public class ClassicTestConstants {
   final static public String ISO_REGEX = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}";
   //pool-1-thread-47
   final static public String NAKED_MAIN_REGEX = ".+";
-
-
   final static public String MAIN_REGEX = "\\[" + NAKED_MAIN_REGEX + "\\]";
   final static public String TEST_PREFIX = CoreTestConstants.BASE_DIR + "src/test/";
   final static public String INPUT_PREFIX = TEST_PREFIX + "input/";
   final static public String RESOURCES_PREFIX = TEST_PREFIX + "resources/";
   final static public String JORAN_INPUT_PREFIX = INPUT_PREFIX + "joran/";
   final static public String ISSUES_PREFIX =   ClassicTestConstants.JORAN_INPUT_PREFIX+"issues/";
-  final static public String GAFFER_INPUT_PREFIX = INPUT_PREFIX + "gaffer/";
   final static public String OUTPUT_DIR_PREFIX= CoreTestConstants.OUTPUT_DIR_PREFIX;
 }
