60969c3ae3a5db5e2de83354d50b965a3f139940
Tony Trinh <tony19@gmail.com>
2016-04-03 11:27:06 -0500

    :zap: Cleanup release.sh

--- release.sh
+++ release.sh
@@ -14,55 +14,15 @@
 # as published by the Free Software Foundation.
 ##############################################################################
 
-# This script performs a logback-android release:
-#  * sets the version (removes SNAPSHOT suffix from current version,
-#    and bumps to the next SNAPSHOT version when release is finished)
-#  * tags the source in GitHub (e.g., with "v_1.0.2")
-#  * deploys the signed jars to Sonatype
-#  * updates the README.md with the current version info
-#  * updates GitHub pages with javadocs and current version info
-#  * generates the javadoc and uploads it to GitHub pages
-#
-# To run this script, simply run:
-#   $ ./release.sh
-#
-
-# Release version "x.x.x-N"
-#
-# where
-#	x.x.x is the version of logback on which logback-android is based
-# and
-#	N is the integral release number of logback-android.
-#
 . gradle.properties
 version=${baseVersion}-${buildVersion}
-outf=logback-android-${version}.jar
 
 echo "Starting release process for logback-android ${version}..."
+
 ./gradlew -Pver=${version}  \
             clean           \
             readme          \
             release         \
             uploadArchives  \
+            docs            \
             shadowJar
-
-echo "Generating javadoc..."
-./gradlew javadocs -Pver=${version}
-
-# Update the web pages
-git clone -b gh-pages https://github.com/tony19/logback-android.git gh-pages
-cd gh-pages
-rm -rf doc/${version}
-mv $PWD/build/docs/javadoc doc/${version}
-
-echo "Updating index.html..."
-gsed -i -e "s/logback-android-[^j]*\.jar/${outf}/g" \
--e "s/[0-9]\+\.[0-9]\+\.[0-9]\+-[0-9]\+/${version}/g" index.html
-
-git add index.html
-git add doc/${version}
-
-# checkin changes to the web pages
-git commit -m "release ${version}"
-
-echo Done. Push changes to GitHub!!
