6d91e695b10307a76e307de36b5021090292a683
Tony Trinh <tony19@gmail.com>
2016-04-07 15:16:02 -0500

    :zap: Tweak release.sh
    * Fix versions
    * Disable uploadDocs (broken)
    * Disable release-asset attachment (broken)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,47 @@
-#!/bin/sh -e
+#!/usr/bin/env bash -e
 
 . gradle.properties
-version=${baseVersion}-${buildVersion}
 
-echo "Starting release process for logback-android ${version}..."
+version=${version%*-SNAPSHOT}
+baseVersion=${version%*-*}
+nextBuild=$((${version##*-} + 1))
+nextVersion="${baseVersion}-${nextBuild}-SNAPSHOT"
 
-./gradlew -Pversion=${version}  \
-            -Ppush              \
-            clean               \
-            readme              \
-            release             \
-            uploadArchives      \
-            uploadDocs          \
+echo "Starting release for logback-android ${version}..."
+
+# gradle-release-plugin prompts for your Nexus credentials
+# with "Please specify username" (no mention of Nexus).
+# Use our own prompt to remind the user where they're
+# logging into to.
+user=${NEXUS_USERNAME}
+pass=${NEXUS_PASSWORD}
+[ -z "$user" ] && read -p "Nexus username: " user
+[ -z "$pass" ] && read -p "Nexus password: " -s pass
+echo ''
+
+./gradlew   -Prelease.useAutomaticVersion=true  \
+            -Prelease.releaseVersion=${version} \
+            -Prelease.newVersion=${nextVersion} \
+            -Pversion=${version}                \
+            -PnexusUsername=${user}             \
+            -PnexusPassword=${pass}             \
+            -Ppush                              \
+            clean                               \
+            readme                              \
+            release                             \
+            uploadArchives                      \
             uberjar
 
-hub release edit -d -m '' ${version} -a build/logback-android-${version}.jar
+echo ''
+echo '/**********************/'
+echo ''
+
+# FIXME: In test repo, this can't checkout 'gh-pages' -- no error provided
+#./gradlew   uploadDocs
+echo TODO: upload javadocs to gh-pages with:
+echo ./gradlew uploadDocs
+
+# FIXME: hub is no longer able to find tagged releases for some reason.
+#hub release edit -m '' v_${version} -a build/logback-android-${version}.jar
+echo TODO: attach uber jar to release at:
+echo https://github.com/tony19/logback-android/releases/tag/v_${version%*-SNAPSHOT}
