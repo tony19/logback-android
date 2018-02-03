bf35c9d1af47ff7956289abf2b6a0ed2b236a579
Tony Trinh <tony19@gmail.com>
2018-02-03 16:02:22 -0600

    Update deploysnapshot.sh to use gradle

--- scripts/deploysnapshot.sh
+++ scripts/deploysnapshot.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
-. gradle.properties
 
-[[ "$TRAVIS" == true ]] && settings='--settings config/travisMavenSettings.xml' || settings=''
-
-mvn versions:set -DnewVersion=${VERSION_NAME}
-
-mvn clean deploy $settings              \
-    -Pdebug                             \
-    -B                                  \
-    -DskipTests=true                    \
-    -Dslf4jVersion=$slf4jVersion
-
-mvn versions:revert
+NEXUS_USERNAME=${NEXUS_USERNAME?} \
+NEXUS_PASSWORD=${NEXUS_PASSWORD?} \
+./gradlew uploadArchives -x test
