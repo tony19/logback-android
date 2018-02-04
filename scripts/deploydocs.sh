40863a706c750e99b773059a9eb848dc875d8759
Tony Trinh <tony19@gmail.com>
2018-02-04 04:24:34 -0600

    Update deploydocs.sh

--- scripts/deploydocs.sh
+++ scripts/deploydocs.sh
@@ -1,8 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
+
+
+uploadJavadoc() {
+    version=${1?}
+    oldbranch=$(git rev-parse --abbrev-ref HEAD)
+    git checkout gh-pages
+    cp -rf logback-android/build/docs/javadoc doc/${version}
+    git add doc/${version}
+    git commit -m ":books: Add javadoc for ${version}"
+    #git push origin gh-pages
+    git checkout ${oldbranch}
+}
 
 # Generate javadoc for specified version...
 if [ -n "$1" ]; then
-    ./gradlew uploadDocs -Pversion="$1"
+    ./gradlew generateReleaseJavadoc -Pversion="$1"
+    uploadJavadoc "$1"
     exit 0
 fi
 
@@ -23,7 +36,8 @@ ${version}
 for v in ${versions}; do
     read -p "Release javadoc for ${v}? [y|N] " prompt
     if [ "$prompt" == "y" ]; then
-        ./gradlew uploadDocs -Pversion=${v}
+        ./gradlew generateReleaseJavadoc -Pversion=${v}
+        uploadJavadoc "${v}"
         exit 0
     fi
 done
