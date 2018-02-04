d6309457eae1c2fe3be2112805fbb5e3d3c9d32c
Tony Trinh <tony19@gmail.com>
2018-02-03 18:03:00 -0600

    Fix number conversion lint warnings

--- logback-android/src/main/java/ch/qos/logback/core/rolling/helper/FileFilterUtil.java
+++ logback-android/src/main/java/ch/qos/logback/core/rolling/helper/FileFilterUtil.java
@@ -107,7 +107,7 @@ public class FileFilterUtil {
           + "] should match [" + lastFileName + "]");
     }
     String counterAsStr = m.group(1);
-    return new Integer(counterAsStr).intValue();
+    return Integer.valueOf(counterAsStr);
   }
 
   public static String slashify(String in) {
