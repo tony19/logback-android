d33897c84930e9ce01f627adb267184a239402b9
Tony Trinh <tony19@gmail.com>
2018-02-06 01:02:35 -0600

    Prompt for credentials in deploysnapshots.sh

--- scripts/deploysnapshot.sh
+++ scripts/deploysnapshot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-user=${NEXUS_USERNAME?}
-pass=${NEXUS_PASSWORD?}
+user=${NEXUS_USERNAME}
+pass=${NEXUS_PASSWORD}
+[ -z "$user" ] && read -p "Nexus username: " user
+[ -z "$pass" ] && read -p "Nexus password: " -s pass
+echo ''
+
 ./gradlew uploadArchives -x test -x build -PNEXUS_USERNAME=${user} -PNEXUS_PASSWORD=${pass}
