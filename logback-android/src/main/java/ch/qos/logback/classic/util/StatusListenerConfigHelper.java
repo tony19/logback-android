064c6c63b7bafb6969a8dd99fa3fe93222391a51
Tony Trinh <tony19@gmail.com>
2018-03-26 13:21:12 -0500

    fix LOGBACK-976 (take2)
    https://github.com/qos-ch/logback/commit/7f1765807

--- logback-android/src/main/java/ch/qos/logback/classic/util/StatusListenerConfigHelper.java
+++ logback-android/src/main/java/ch/qos/logback/classic/util/StatusListenerConfigHelper.java
@@ -32,10 +32,10 @@ public class StatusListenerConfigHelper {
   private static void addStatusListener(LoggerContext loggerContext,
       String listenerClass) {
     StatusListener listener = createListenerPerClassName(loggerContext, listenerClass);
-    initListener(loggerContext, listener);
+    initAndAddListener(loggerContext, listener);
   }
 
-  private static void initListener(LoggerContext loggerContext, StatusListener listener) {
+  private static void initAndAddListener(LoggerContext loggerContext, StatusListener listener) {
     if (listener != null) {
       if(listener instanceof ContextAware) // LOGBACK-767
         ((ContextAware) listener).setContext(loggerContext);
--- logback-android/src/test/java/ch/qos/logback/core/BasicStatusManagerTest.java
+++ logback-android/src/test/java/ch/qos/logback/core/BasicStatusManagerTest.java
@@ -63,22 +63,43 @@ public class BasicStatusManagerTest {
   @Test
   public void many() {
     int margin = 300;
-    int len = MAX_HEADER_COUNT+TAIL_SIZE+margin;
-    for(int i = 0; i < len; i++) {
-      bsm.add(new ErrorStatus(""+i, this));
+    int len = MAX_HEADER_COUNT + TAIL_SIZE + margin;
+    for (int i = 0; i < len; i++) {
+      bsm.add(new ErrorStatus("" + i, this));
     }
 
     List<Status> statusList = bsm.getCopyOfStatusList();
     assertNotNull(statusList);
-    assertEquals(MAX_HEADER_COUNT+TAIL_SIZE, statusList.size());
+    assertEquals(MAX_HEADER_COUNT + TAIL_SIZE, statusList.size());
     List<Status> witness = new ArrayList<Status>();
-    for(int i = 0; i < MAX_HEADER_COUNT; i++) {
-      witness.add(new ErrorStatus(""+i, this));
+    for (int i = 0; i < MAX_HEADER_COUNT; i++) {
+      witness.add(new ErrorStatus("" + i, this));
     }
-    for(int i = 0; i < TAIL_SIZE; i++) {
-      witness.add(new ErrorStatus(""+(MAX_HEADER_COUNT+margin+i), this));
+    for (int i = 0; i < TAIL_SIZE; i++) {
+      witness.add(new ErrorStatus("" + (MAX_HEADER_COUNT + margin + i), this));
     }
     assertEquals(witness, statusList);
+  }
+
+  @Test
+  public void duplicateInstallationsOfOnConsoleListener() {
+    OnConsoleStatusListener sl0 = new OnConsoleStatusListener();
+    sl0.start();
+    OnConsoleStatusListener sl1 = new OnConsoleStatusListener();
+    sl1.start();
+
+    bsm.add(sl0);
+
+    {
+      List<StatusListener> listeners = bsm.getCopyOfStatusListenerList();
+      assertEquals(1, listeners.size());
+    }
+
+    bsm.add(sl1);
+    {
+      List<StatusListener> listeners = bsm.getCopyOfStatusListenerList();
+      assertEquals(1, listeners.size());
+    }
   }
 
   @Test
