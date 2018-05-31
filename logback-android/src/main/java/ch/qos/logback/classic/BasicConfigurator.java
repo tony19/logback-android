509ee94c8f6a3ea886d1f0b9b9f9e480cb11ee2e
Tony Trinh <tony19@gmail.com>
2018-05-31 00:36:55 -0500

    fixed LOGBACK-1142
    https://github.com/qos-ch/logback/commit/43353eb1d

--- logback-android/src/main/java/ch/qos/logback/classic/BasicConfigurator.java
+++ logback-android/src/main/java/ch/qos/logback/classic/BasicConfigurator.java
@@ -13,19 +13,17 @@
  */
 package ch.qos.logback.classic;
 
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.layout.TTLLLayout;
 import ch.qos.logback.classic.spi.Configurator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
 import ch.qos.logback.core.spi.ContextAwareBase;
-import ch.qos.logback.core.status.InfoStatus;
-import ch.qos.logback.core.status.StatusManager;
 
 /**
  * BasicConfigurator configures logback-classic by attaching a
  * {@link ConsoleAppender} to the root logger. The console appender's layout
- * is set to a {@link  PatternLayout} with the pattern
- * "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n".
+ * is set to a {@link ch.qos.logback.classic.layout.TTLLLayout TTLLLayout}.
  *
  * @author Ceki Gulcu
  * @deprecated See {@link ch.qos.logback.classic.android.BasicLogcatConfigurator}.
@@ -37,19 +35,23 @@ public class BasicConfigurator extends ContextAwareBase implements Configurator 
   }
 
   public void configure(LoggerContext lc) {
-    StatusManager sm = lc.getStatusManager();
-    if(sm != null)  {
-     sm.add(new InfoStatus("Setting up default configuration.", lc));
-    }
+    addInfo("Setting up default configuration.");
     ConsoleAppender<ILoggingEvent> ca = new ConsoleAppender<ILoggingEvent>();
     ca.setContext(lc);
     ca.setName("console");
-    PatternLayoutEncoder pl = new PatternLayoutEncoder();
-    pl.setContext(lc);
-    pl.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
-    pl.start();
+    LayoutWrappingEncoder<ILoggingEvent> encoder = new LayoutWrappingEncoder<ILoggingEvent>();
+    encoder.setContext(lc);
 
-    ca.setEncoder(pl);
+    // same as 
+    // PatternLayout layout = new PatternLayout();
+    // layout.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
+    TTLLLayout layout = new TTLLLayout();
+
+    layout.setContext(lc);
+    layout.start();
+    encoder.setLayout(layout);
+
+    ca.setEncoder(encoder);
     ca.start();
     Logger rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME);
     rootLogger.addAppender(ca);
