2d0af6d7c19c28ba42884ac87357dae222900ab0
Tony Trinh <tony19@gmail.com>
2018-06-13 04:54:47 -0500

    ongoing work on tests
    https://github.com/qos-ch/logback/commit/faeb5e086

--- logback-android/src/test/java/ch/qos/logback/core/rolling/TimeBasedRollingWithArchiveRemoval_Test.java
+++ logback-android/src/test/java/ch/qos/logback/core/rolling/TimeBasedRollingWithArchiveRemoval_Test.java
@@ -13,6 +13,7 @@
  */
 package ch.qos.logback.core.rolling;
 
+import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.pattern.SpacePadder;
 import ch.qos.logback.core.rolling.helper.RollingCalendar;
 import ch.qos.logback.core.util.FixedRateInvocationGate;
@@ -23,9 +24,18 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
 
 import static ch.qos.logback.core.CoreConstants.DAILY_DATE_PATTERN;
 import static org.junit.Assert.assertEquals;
@@ -50,6 +60,7 @@ public class TimeBasedRollingWithArchiveRemoval_Test extends ScaffoldingForRolli
 
   // Wed Mar 23 23:07:05 CET 2016
   static final long WED_2016_03_23_T_230705_CET = 1458770825333L;
+  static final long THU_2016_03_17_T_230330_CET = 1458252210975L;
 
   int slashCount = 0;
   int ticksPerPeriod = 216;
@@ -101,15 +112,37 @@ public class TimeBasedRollingWithArchiveRemoval_Test extends ScaffoldingForRolli
     checkFileCount(expectedCountWithFolders(maxHistory, withExtraFolder));
   }
 
-  void generateDailyRollover(ConfigParameters cp) {
+  long generateDailyRollover(ConfigParameters cp) {
     this.slashCount = computeSlashCount(DAILY_DATE_PATTERN);
     cp.fileNamePattern(randomOutputDir + "clean-%d{" + DAILY_DATE_PATTERN + "}.txt");
-    logOverMultiplePeriods(cp);
+    return logOverMultiplePeriods(cp);
   }
 
-  void generateDailyRolloverAndCheckFileCount(ConfigParameters cp) {
-    generateDailyRollover(cp);
-    checkFileCount(expectedCountWithoutFoldersWithInactivity(cp.maxHistory, cp.simulatedNumberOfPeriods, cp.startInactivity + cp.numInactivityPeriods));
+  long generateDailyRolloverAndCheckFileCount(ConfigParameters cp) {
+    long millisAtEnd = generateDailyRollover(cp);
+    int periodBarriersCrossed = computeCrossedDayBarriers(currentTime, millisAtEnd);
+    checkFileCount(expectedCountWithoutFoldersWithInactivity(cp.maxHistory, periodBarriersCrossed, cp.startInactivity + cp.numInactivityPeriods));
+    return millisAtEnd;
+  }
+
+  // FIXME: Fix failing test
+  @Ignore
+  @Test
+  public void checkCrossedPeriods() {
+    long SAT_2016_03_26_T_230705_CET = WED_2016_03_23_T_230705_CET + 3 * CoreConstants.MILLIS_IN_ONE_DAY;
+    System.out.println("SAT_2016_03_26_T_230705_CET " + new Date(SAT_2016_03_26_T_230705_CET));
+    long MON_2016_03_28_T_000705_CET = SAT_2016_03_26_T_230705_CET + CoreConstants.MILLIS_IN_ONE_DAY;
+    System.out.println("MON_2016_03_28_T_000705_CET " + new Date(MON_2016_03_28_T_000705_CET));
+
+    int result = computeCrossedDayBarriers(SAT_2016_03_26_T_230705_CET, MON_2016_03_28_T_000705_CET);
+    assertEquals(2, result);
+  }
+
+  private int computeCrossedDayBarriers(long currentTime, long millisAtEnd) {
+    LocalDate startInstant = new LocalDate(currentTime, DateTimeZone.getDefault());
+    LocalDate endInstant = new LocalDate(millisAtEnd, DateTimeZone.getDefault());
+    Days days = Days.daysBetween(startInstant, endInstant);
+    return days.getDays();
   }
 
   @Test
@@ -151,7 +184,8 @@ public class TimeBasedRollingWithArchiveRemoval_Test extends ScaffoldingForRolli
 
   @Test
   public void checkCleanupForDailyRolloverWithInactivity_10Periods() {
-    cp.maxHistory(6).simulatedNumberOfPeriods(10).startInactivity(3).numInactivityPeriods(4);
+    this.currentTime = THU_2016_03_17_T_230330_CET;
+    cp.maxHistory(6).simulatedNumberOfPeriods(10).startInactivity(2).numInactivityPeriods(2);
     generateDailyRolloverAndCheckFileCount(cp);
   }
 
