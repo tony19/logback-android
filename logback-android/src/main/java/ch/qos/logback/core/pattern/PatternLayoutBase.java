e3f639ddb965ca365eea20b91520a67e28c04c6b
Tony Trinh <tony19@gmail.com>
2018-06-15 02:27:03 -0500

    increase initial StringBuilder size
    https://github.com/qos-ch/logback/commit/0aaf15707

--- logback-android/src/main/java/ch/qos/logback/core/pattern/PatternLayoutBase.java
+++ logback-android/src/main/java/ch/qos/logback/core/pattern/PatternLayoutBase.java
@@ -28,6 +28,8 @@ import java.util.Map;
 
 abstract public class PatternLayoutBase<E> extends LayoutBase<E> {
 
+  static final int INTIAL_STRING_BUILDER_SIZE = 256;
+
   Converter<E> head;
   String pattern;
   protected PostCompileProcessor<E> postCompileProcessor;
@@ -113,7 +115,7 @@ abstract public class PatternLayoutBase<E> extends LayoutBase<E> {
   }
 
   protected String writeLoopOnConverters(E event) {
-    StringBuilder buf = new StringBuilder(128);
+    StringBuilder buf = new StringBuilder(INTIAL_STRING_BUILDER_SIZE);
     Converter<E> c = head;
     while (c != null) {
       c.write(buf, event);
