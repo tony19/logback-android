619ef8ef0c0dc81462db232266b603baec13fc1a
Tony Trinh <tony19@gmail.com>
2018-06-16 02:53:42 -0500

    minor edit
    https://github.com/qos-ch/logback/commit/45784a52d

--- logback-android/src/test/java/ch/qos/logback/core/pattern/parser/AbstractPatternLayoutBaseTest.java
+++ logback-android/src/test/java/ch/qos/logback/core/pattern/parser/AbstractPatternLayoutBaseTest.java
@@ -15,7 +15,6 @@ package ch.qos.logback.core.pattern.parser;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
 
 import org.junit.Test;
 
