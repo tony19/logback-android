41ca096439aa46247004f525ad428df364c37a8d
Tony Trinh <tony19@gmail.com>
2019-10-04 05:18:14 -0500

    Add dsl for file appenders

--- logback-android/src/main/java/ch/qos/logback/core/dsl/Appender.kt
+++ logback-android/src/main/java/ch/qos/logback/core/dsl/Appender.kt
@@ -4,6 +4,11 @@ import ch.qos.logback.classic.android.LogcatAppender
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.Appender
+import ch.qos.logback.core.FileAppender
+import ch.qos.logback.core.encoder.Encoder
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.RollingPolicy
+import ch.qos.logback.core.rolling.TriggeringPolicy
 
 fun <T> Configuration.appender(appender: () -> T, block: T.() -> Unit = {})
     where T: Appender<ILoggingEvent> {
@@ -40,3 +45,44 @@ fun Configuration.logcatAppender(name: String = "logcat", block: LogcatAppender.
         block()
     }
 }
+
+fun <E: ILoggingEvent> FileAppender<E>.encoder(pattern: String) {
+    val context = this.context
+    @Suppress("UNCHECKED_CAST")
+    encoder = PatternLayoutEncoder().apply {
+        this.pattern = pattern
+        this.context = context
+        start()
+    } as Encoder<E>
+}
+
+fun <E: ILoggingEvent> FileAppender<E>.file(name: String) {
+    // TODO: Automatically convert relative path to absolute local-file path
+    // Android requires absolute path
+    file = name
+}
+
+fun <E: ILoggingEvent> RollingFileAppender<E>.encoder(pattern: String) {
+    val context = this.context
+    @Suppress("UNCHECKED_CAST")
+    encoder = PatternLayoutEncoder().apply {
+        this.pattern = pattern
+        this.context = context
+        start()
+    } as Encoder<E>
+}
+
+fun <E: ILoggingEvent> RollingFileAppender<E>.file(name: String) {
+    // TODO: Automatically convert relative path to absolute local-file path
+    // Android requires absolute path
+    file = name
+}
+
+fun <E: ILoggingEvent, R: RollingPolicy> RollingFileAppender<E>.rollingPolicy(policy: () -> R, block: R.() -> Unit = {}) {
+    rollingPolicy = policy().apply(block)
+}
+
+fun <E: ILoggingEvent, R: TriggeringPolicy<E>> RollingFileAppender<E>.triggeringPolicy(policy: () -> R, block: R.() -> Unit = {}) {
+    triggeringPolicy = policy().apply(block)
+}
+
