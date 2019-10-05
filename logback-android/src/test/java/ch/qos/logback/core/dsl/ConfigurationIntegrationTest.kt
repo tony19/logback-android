040d3dbdf6459050f7f0fcb6d6f2cabbf743a70f
Tony Trinh <tony19@gmail.com>
2019-10-05 02:59:16 -0500

    Add integration tests for Configuration

--- logback-android/src/test/java/ch/qos/logback/core/dsl/ConfigurationIntegrationTest.kt
+++ logback-android/src/test/java/ch/qos/logback/core/dsl/ConfigurationIntegrationTest.kt
@@ -1,2 +1,29 @@
 package ch.qos.logback.core.dsl
 
+import io.kotlintest.specs.FreeSpec
+
+class ConfigurationIntegrationTest: FreeSpec({
+    "logs events to logcat" {
+        val x = Configuration {
+            debug(true)
+            root {
+                logcatAppender()
+            }
+        }
+        x.context.getLogger(ConfigurationIntegrationTest::class.simpleName).info("hello world")
+    }
+
+    "logs events to file" {
+        val x = Configuration {
+            debug(true)
+            root {
+                fileAppender()
+            }
+        }
+        x.context.getLogger(ConfigurationIntegrationTest::class.simpleName).info("hello world")
+    }
+
+    "logs events to rolling files" {
+
+    }
+})
