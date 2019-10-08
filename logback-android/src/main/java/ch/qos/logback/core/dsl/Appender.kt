87a004e33a93abe50a63adcefe572b1582098923
Tony Trinh <tony19@gmail.com>
2019-10-08 17:12:00 -0500

    Remove unused imports from Configuration dsl

--- logback-android/src/main/java/ch/qos/logback/core/dsl/Appender.kt
+++ logback-android/src/main/java/ch/qos/logback/core/dsl/Appender.kt
@@ -1,13 +1,7 @@
 package ch.qos.logback.core.dsl
 
-import ch.qos.logback.classic.android.LogcatAppender
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.Appender
-import ch.qos.logback.core.FileAppender
-import ch.qos.logback.core.encoder.Encoder
-import ch.qos.logback.core.rolling.*
-import ch.qos.logback.core.util.FileSize
 
 fun <T> Configuration.appender(appender: () -> T, block: T.() -> Unit = {})
     where T: Appender<ILoggingEvent> {
