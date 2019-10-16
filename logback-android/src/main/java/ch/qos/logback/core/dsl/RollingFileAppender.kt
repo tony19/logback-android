643d8be791696ee85cd7b09741fd7b25f2b366df
Tony Trinh <tony19@gmail.com>
2019-10-16 01:47:40 -0500

    Remove unused import

--- logback-android/src/main/java/ch/qos/logback/core/dsl/RollingFileAppender.kt
+++ logback-android/src/main/java/ch/qos/logback/core/dsl/RollingFileAppender.kt
@@ -3,7 +3,6 @@ package ch.qos.logback.core.dsl
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.classic.spi.ILoggingEvent
-import ch.qos.logback.core.FileAppender
 import ch.qos.logback.core.encoder.Encoder
 import ch.qos.logback.core.rolling.*
 import ch.qos.logback.core.util.FileSize
