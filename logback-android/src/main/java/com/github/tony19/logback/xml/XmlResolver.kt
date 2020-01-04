eb4f385ce3d5c687528dad4a64ed243ea14bc89f
Tony Trinh <tony19@gmail.com>
2020-01-03 22:49:45 -0600

    refactor: Move blocks into XmlResolver methods

--- logback-android/src/main/java/com/github/tony19/logback/xml/XmlResolver.kt
+++ logback-android/src/main/java/com/github/tony19/logback/xml/XmlResolver.kt
@@ -2,6 +2,7 @@ package com.github.tony19.logback.xml
 
 import com.gitlab.mvysny.konsumexml.Konsumer
 import com.gitlab.mvysny.konsumexml.anyName
+import java.lang.reflect.Method
 import java.nio.charset.Charset
 import java.util.*
 
@@ -17,47 +18,52 @@ class XmlResolver {
             val instMethods = inst.javaClass.methods
             k.children(anyName) {
                 if (name?.localPart?.isNotEmpty()!!) {
-                    val elemName = name?.localPart?.toLowerCase(Locale.US)
-                    // Prioritize adder in case a setter exists for an array. The
-                    // adder adds a single param, which works with the flow below.
-                    val setterMethod = instMethods
-                            .find {
-                                it.name.toLowerCase(Locale.US) == "add${elemName}"
-                                && it.parameterTypes.size == 1 }
-                        ?: instMethods
-                            .find {
-                                it.name.toLowerCase(Locale.US) == "set${elemName}"
-                                && it.parameterTypes.size == 1 }
-
+                    val setterMethod = findSetterMethod(instMethods, name?.localPart)
                     if (setterMethod == null) {
                         skipContents()
                         println("warning: setter not found: \"set${name!!.localPart.capitalize()}\" or \"add${name!!.localPart.capitalize()}\"")
 
                     } else {
-                        val paramType = setterMethod.parameterTypes[0]
-                        val value = when {
-                            paramType.name == "java.lang.String" -> text()
-                            paramType.name == "java.nio.charset.Charset" -> text { Charset.forName(it) }
-                            paramType.isPrimitive -> text { parsePrimitive(paramType, it)!! }
-                            else -> {
-                                val className = attributes.getValueOpt("class")
-                                val param = if (className != null && className.isNotEmpty()) Class.forName(className) else paramType
-
-                                val paramInst = try {
-                                    param.getDeclaredConstructor().newInstance()
-                                } catch(e: NoSuchMethodException) {
-                                    println("warning: missing default constructor for \"${param.name}\"")
-                                    throw e
-                                }
-
-                                resolve(this, paramInst).apply {
-                                    // FIXME: We need to have LoggerContext set before calling start()
-                                    //javaClass.methods.find { it.name == "start" }?.invoke(this)
-                                }
-                            }
-                        }
+                        val value = resolveValue(this, setterMethod.parameterTypes[0])
                         setterMethod.invoke(inst, value)
                     }
+                }
+            }
+        }
+    }
+
+    private fun findSetterMethod(instMethods: Array<Method>, elemLocalPartName: String?): Method? {
+        val elemName = elemLocalPartName?.toLowerCase(Locale.US)
+        val find: (String) -> Method? = { prefix ->
+            instMethods.find {
+                it.name.toLowerCase(Locale.US) == "${prefix}${elemName}"
+                && it.parameterTypes.size == 1
+            }
+        }
+        // Prioritize adder in case a setter exists for an array. The adder
+        // adds a single param, which works with the flow in resolveValue().
+        return find("add") ?: find("set")
+    }
+
+    private fun resolveValue(k: Konsumer, paramType: Class<*>): Any {
+        return when {
+            paramType.name == "java.lang.String" -> k.text()
+            paramType.name == "java.nio.charset.Charset" -> k.text { Charset.forName(it) }
+            paramType.isPrimitive -> k.text { parsePrimitive(paramType, it)!! }
+            else -> {
+                val className = k.attributes.getValueOpt("class")
+                val param = if (className != null && className.isNotEmpty()) Class.forName(className) else paramType
+
+                val paramInst = try {
+                    param.getDeclaredConstructor().newInstance()
+                } catch(e: NoSuchMethodException) {
+                    println("warning: missing default constructor for \"${param.name}\"")
+                    throw e
+                }
+
+                resolve(k, paramInst).apply {
+                    // FIXME: We need to have LoggerContext set before calling start()
+                    //javaClass.methods.find { it.name == "start" }?.invoke(this)
                 }
             }
         }
