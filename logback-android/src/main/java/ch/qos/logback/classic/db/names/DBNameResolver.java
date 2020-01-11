2de28256033a4ee731e8375ee510745e7df03419
Tony Trinh <tony19@gmail.com>
2020-01-11 02:17:47 -0600

    chore: Auto-update license headers
    Used licenseheaders.py with minor tweaks
    
    https://github.com/johann-petrak/licenseheaders.py

--- logback-android/src/main/java/ch/qos/logback/classic/db/names/DBNameResolver.java
+++ logback-android/src/main/java/ch/qos/logback/classic/db/names/DBNameResolver.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2019 Anthony Trinh
+/*
+ * Copyright (c) 2020 Anthony Trinh.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
