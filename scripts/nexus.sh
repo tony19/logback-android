217c13cad2dd85e582b840df2ffb08ccfdac71d0
Tony Trinh <tony19@gmail.com>
2022-11-11 23:41:13 -0600

    build: disable build cache for release build

--- scripts/nexus.sh
+++ scripts/nexus.sh
@@ -15,4 +15,6 @@
 # ossrhPassword=<Sonatype JIRA password
 # sonatypeStagingProfileId=b2413418ab44f
 
-./gradlew logback-android:publishReleasePublicationToSonatypeRepository
+# TODO: Test this: To close and release the non-snapshot, use this command: ./gradlew logback-android:publishToSonatype logback-android:closeAndReleaseSonatypeStagingRepository
+# But we need to do that only if version does not end with -SNAPSHOT. Also, do we continue using the other nexus-staging relase plugin to manage the git tagging, and updating the version?
+./gradlew logback-android:publishToSonatype --no-configuration-cache
