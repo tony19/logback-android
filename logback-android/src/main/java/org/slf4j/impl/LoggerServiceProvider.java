1730205402859f6418195e390db2b108a82e99fc
Tony Trinh <tony19@gmail.com>
2022-12-04 15:01:28 -0600

    fix: update requested slf4j api version in LoggerServiceProvider

--- logback-android/src/main/java/org/slf4j/impl/LoggerServiceProvider.java
+++ logback-android/src/main/java/org/slf4j/impl/LoggerServiceProvider.java
@@ -26,7 +26,7 @@ public class LoggerServiceProvider implements SLF4JServiceProvider {
      * against. The value of this field is usually modified with each release.
      */
     // to avoid constant folding by the compiler, this field must *not* be final
-    public static String REQUESTED_API_VERSION = "2.0.3"; // !final
+    public static String REQUESTED_API_VERSION = "2.0.5"; // !final
     private LoggerContext defaultLoggerContext;;
     private IMarkerFactory markerFactory;
     private MDCAdapter mdcAdapter;
